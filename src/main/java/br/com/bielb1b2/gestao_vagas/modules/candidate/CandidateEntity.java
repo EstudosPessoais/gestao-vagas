51cea2ec90fcd9dc28a1d0afbbffa8577e331efc
bielb1b2 <gabrielvt.souza@hotmail.com>
2024-12-13 15:41:35 -0300

    feat: add error handler

--- src/main/java/br/com/bielb1b2/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/bielb1b2/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,6 +1,10 @@
 package br.com.bielb1b2.gestao_vagas.modules.candidate;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.UUID;
 
@@ -9,8 +13,15 @@ public class CandidateEntity {
 
     private UUID id;
     private String name;
+
+    @NotBlank(message = "O campo [username] não deve ser vazio")
+    @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço")
     private String username;
+
+    @Email(message = "O campo [email] deve conter um e-mail válido")
     private String email;
+
+    @Length(min = 10, max = 100, message = "O campo [senha] deve ter comprimento entre 10 e 100 caracteres")
     private String password;
     private String description;
     private String curriculum;
